47977de181e7f4c53df648dd74dac37385095fbb
psarirafraz <parsasarirafraz@live.com>
2020-11-13 02:04:53 -0500

    adding DateTimePicker

--- screens/stockProfile.js
+++ screens/stockProfile.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect  } from 'react';
 import { StyleSheet, Text } from 'react-native';
-import EarningChart from '../shared/EarningChart.js';
-
+// import EarningChart from '../shared/EarningChart.js';
+import DateTimePicker from '@react-native-community/datetimepicker'
 
 export default function stockProfile({ navigation }) {
-    const sym = navigation.getParam('symbol')
+    // const sym = navigation.getParam('symbol')
+    const [date,setDate] = useState(new Date());
     return (
-        <EarningChart symbol={sym}></EarningChart> 
+        <DateTimePicker 
+        value={ date }
+        mode='datetime'
+        display='default'
+        onChange={ date => setDate(date) } 
+        />
     );
 }
   
